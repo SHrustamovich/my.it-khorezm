e39640ad9719a84ebac7f7932d70a71cb44c8439
shohruhrustamovich5666@gmail.com <shohruhrustamovich5666@gmail.com>
2023-09-06 06:41:18 -0700

    added https

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import Root from "./components/root.jsx";
+// import Root from "./components/root.jsx";
 import "./assets/style/main.scss";
+import LoginPage from "./pages/login/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Root />
-  </BrowserRouter>
+    <BrowserRouter>
+        <LoginPage />
+    </BrowserRouter>
 );
